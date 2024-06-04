e97b2b596717be6a4684e580ec70db50522a5b18
Ash Whiting <ash_whiting@hotmail.com>
2024-06-04 16:28:41 +0100

    Initial commit

---
+++ wp-disable-comments.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Plugin Name: BrightLocal - Completely Disable Comments
+ * Description: Away with you - pesky comments
+ * Version: 1.0.0
+ * Author: Ash Whiting for BrightLocal
+ * Author URI: https://brightlocal.com
+ * Text Domain: bl-disable-comments
+ * GitHub Plugin URI: https://github.com/asha23/wp-completely-disable-comments
+ * Primary Branch: main
+ * Release Asset: true
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+add_action('admin_init', function () {
+
+    global $pagenow;
+     
+    if ($pagenow === 'edit-comments.php') {
+        wp_safe_redirect(admin_url());
+        exit;
+    }
+ 
+    // Remove comments metabox from dashboard
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+ 
+    // Disable support for comments and trackbacks in post types
+    foreach (get_post_types() as $post_type) {
+        if (post_type_supports($post_type, 'comments')) {
+            remove_post_type_support($post_type, 'comments');
+            remove_post_type_support($post_type, 'trackbacks');
+        }
+    }
+});
+ 
+// Close comments on the front-end
+add_filter('comments_open', '__return_false', 20, 2);
+add_filter('pings_open', '__return_false', 20, 2);
+ 
+// Hide existing comments
+add_filter('comments_array', '__return_empty_array', 10, 2);
+ 
+// Remove comments page in menu
+add_action('admin_menu', function () {
+    remove_menu_page('edit-comments.php');
+});
+ 
+// Remove comments links from admin bar
+add_action('init', function () {
+    if (is_admin_bar_showing()) {
+        remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+    }
+});
